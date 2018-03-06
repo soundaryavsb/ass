f4e25fffbc25027f2e722ab529c93fdc1f599f37
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-03-06 20:48:22 +0530

    Update 2q.c

--- 2q.c
+++ 2q.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[10],i,s=0,f=0;
+	int a[10],i,f=0,s=0;
 	for(i=0;i<10;i++)
 	{
 		scanf("%d",&a[i]);
 	}
 	for(i=0;i<10;i++)
 	{
-	if(a[i]>f)
+		if(a[i]>f)
+		{
+			f=a[i];
+		}
+	}
+		for(i=0;i<10;i++)
 	{
-              s=f;
-              f=a[i];
+		if(a[i]>s&&a[i]!=f)
+		{
+			s=a[i];
+		}
 	}
-	}
-	printf("%d \t %d are largest number",s,f);
+	printf("%d,%d are tha largest number");
 	return 0;
 }
